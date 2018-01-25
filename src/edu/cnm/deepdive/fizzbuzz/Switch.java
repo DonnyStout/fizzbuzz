8b620a8091224b5f8ce25772aac5f8eca9d7cc07
DonnyStout <donnystout47@gmail.com>
2018-01-25 11:22:00 -0700

    Initial commit of FizzBuzz

---
+++ src/edu/cnm/deepdive/fizzbuzz/Switch.java
@@ -0,0 +1,40 @@
+package edu.cnm.deepdive.fizzbuzz;
+
+public class Switch {
+
+  /**
+   * Demonstrates the switch statement.
+   */
+  public static void main(String[] args) {
+    String function = args[0].toLowerCase();
+    int a = Integer.parseInt(args[1]);
+    int b = Integer.parseInt(args[2]);
+//    if (function.equals("add")) {
+//      System.out.println(a + b);
+//    } else if (function.equals("subtract")) {
+//      System.out.println(a - b);
+//    } else if (function.equals("multiply")) {
+//      System.out.println("a * b");
+//    } else if (function.equals("divide")) {
+//      System.out.println(a / b);
+//    }
+    switch (function) {
+      case "add":
+        System.out.println(a + b);
+        break;
+      case "subtract":
+        System.out.println(a - b);
+        break;
+      case "multiply":
+        System.out.println(a * b);
+        break;
+      case "divide":
+        System.out.println(a / b);
+        break;
+      default:
+        System.out.println("Not a valid function");
+        break;
+    }
+
+  }
+}
